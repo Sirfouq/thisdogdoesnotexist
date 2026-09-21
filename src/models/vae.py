120314cbd55116f64556a76f0464f40c6adefca9
giorgoskokkinos <kokkinos1@gmail.com>
2026-09-21 10:19:30 +0200

    -Add encoder,  decoder architecture builder functions.

--- src/models/vae.py
+++ src/models/vae.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torchinfo
 
 class Encoder(nn.Module):
     def __init__(self,encoder : nn.Module):
@@ -63,3 +64,48 @@ def vae_loss(batch_size,x_hat, x, mu, log_var):
 
 
 
+def build_encoder_net(image_size, channels, latent_dim):
+    spatial = image_size // (2**4)
+    encoder_net = nn.Sequential(
+        nn.Conv2d(in_channels=channels, out_channels= 64, kernel_size=(4,4), stride=2, padding=1), # stride 0f 2 and padding of 1 ] makes h , w =(64,64) -> h,w = (32,32)
+        nn.GroupNorm(8,64),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4,4), stride=2, padding=1 ),
+        nn.GroupNorm(8,128),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(4,4), stride=2, padding=1 ),
+        nn.GroupNorm(8,256),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(4,4), stride=2, padding=1 ),
+        nn.GroupNorm(8,512),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Flatten(),
+        nn.Linear(512*spatial*spatial, 2*latent_dim)
+    )
+    return encoder_net
+
+
+def build_decoder_net(latent_dim, channels, image_size):
+    spatial = image_size//(2**4)
+    decoder_net = nn.Sequential(
+        nn.Linear(latent_dim,512*spatial*spatial),
+        nn.Unflatten(1,torch.Size([512,spatial,spatial])),
+        nn.GroupNorm(8,512),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=(4,4),stride=2,padding=1),
+        nn.GroupNorm(8,256),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=(4,4),stride=2,padding=1),
+        nn.GroupNorm(8,128),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=(4,4),stride=2,padding=1),
+        nn.GroupNorm(8,64),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=(4,4),stride=2, padding=1),
+        nn.GroupNorm(8,64),
+        nn.LeakyReLU(negative_slope=0.1),
+        nn.Conv2d(in_channels=64, out_channels=channels,kernel_size=(3,3), stride=1, padding=1),
+        nn.Tanh()
+    )
+
+    return decoder_net
